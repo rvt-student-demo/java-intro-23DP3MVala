8b24c4fbd9029e1ca19a2bb010ebbc7b1a7c67df
23DP3MVala <166013386+23DP3MVala@users.noreply.github.com>
2024-09-27 07:58:34 +0000

    Number and sum of numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -86,23 +86,39 @@ public class App
         // }
 
 
-        System.out.println("Value of the gift?");
-        int value = Integer.valueOf(input.nextLine());
+        // System.out.println("Value of the gift?");
+        // int value = Integer.valueOf(input.nextLine());
 
-        if (value >= 1000000) {
-            System.out.println("Tax: " + (142100 + (value - 1000000) * 0.17));
-        } else if (value >= 200000) {
-            System.out.println("Tax: " + (22100 + (value - 200000) * 0.15));
-        } else if (value >= 55000) {
-            System.out.println("Tax: " + (4700 + (value - 55000) * 0.12));
-        } else if (value >= 25000) {
-            System.out.println("Tax: " + (1700 + (value - 25000) * 0.1));
-        } else if (value >= 5000) {
-            System.out.println("Tax: " + (100 + (value - 5000) * 0.08));
-        } else {
-            System.out.println("No tax!");
+        // if (value >= 1000000) {
+        //     System.out.println("Tax: " + (142100 + (value - 1000000) * 0.17));
+        // } else if (value >= 200000) {
+        //     System.out.println("Tax: " + (22100 + (value - 200000) * 0.15));
+        // } else if (value >= 55000) {
+        //     System.out.println("Tax: " + (4700 + (value - 55000) * 0.12));
+        // } else if (value >= 25000) {
+        //     System.out.println("Tax: " + (1700 + (value - 25000) * 0.1));
+        // } else if (value >= 5000) {
+        //     System.out.println("Tax: " + (100 + (value - 5000) * 0.08));
+        // } else {
+        //     System.out.println("No tax!");
+        // }
+        int numbers = 0;
+        int sum = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(input.nextLine());
+
+            if (number == 0) {
+                System.out.println("Number of numbers: "+numbers);
+                System.out.println("Sum of the numbers: "+sum);
+                break;
+            }
+            if (!(number == 0)) {
+                numbers = numbers +1;
+                sum = sum + number;
+            }
         }
-
     }
 
 }
